fba6a9b0242ad6de4bd295f6a96d8ccfcfaf865c
Ryan Putra <vicrisesofficial@gmail.com>
2021-10-22 16:07:04 +0700

    ryan updates models.py :)

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=64)
+    password = models.CharField(max_length=128)
+    name = models.CharField(max_length=40)git
+    DOB = models.DateField()
+    email = models.EmailField()
+    image_link = models.CharField(max_length=32)
+    instagram = models.CharField(max_length=30)
+    line = models.CharField(max_length=32)
+
