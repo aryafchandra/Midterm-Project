124a971ed2a3c1443a5ac6208fffe770910b1140
Ryan Putra <vicrisesofficial@gmail.com>
2021-10-22 16:08:09 +0700

    ryan updates models.py :)

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=64)
     password = models.CharField(max_length=128)
-    name = models.CharField(max_length=40)git
+    name = models.CharField(max_length=40)
     DOB = models.DateField()
     email = models.EmailField()
     image_link = models.CharField(max_length=32)
