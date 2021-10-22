9f25a65eee7e7dd4276b928d841864f29649c8ae
leonardfelix <leonard.felix01@ui.ac.id>
2021-10-22 23:47:39 +0700

    felix update suggestion

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-
-from .views import main, about
+from .views import main, about, suggestion
 
 app_name = 'main'
 
 urlpatterns = [
     path('', main, name='home'),
-    path('about', about, name='about')
+    path('about', about, name='about'),
+    path('suggestion', suggestion, name='suggestion')
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import User
 
+currentname = ""
+currentinterest = "snapping"
 
 def main(request):
     return render(request, 'base.html')
@@ -7,3 +10,22 @@ def main(request):
 def about(request):
     return render(request, 'about.html')
 
+def suggestion(request):
+    #if name == "":
+        #return render(request, 'base.html')
+    #else:
+    currentinterestlst = currentinterest.split(",")
+    recomendedpeople = []
+    users =  User.objects.all().values() # TODO Implement this
+    print(users)
+    for data in users:
+        interest = data.get('interest')
+        interestlst = interest.split(",")
+        print(set(interestlst).intersection(currentinterestlst))
+        if set(interestlst).intersection(currentinterestlst) != set():
+            recomendedpeople.append({'name':data.get('name'), 'interest':interest})
+
+    #set notes as a collection of object Notes
+    response = {'users': recomendedpeople}
+    print(response)
+    return render(request, 'suggestion.html', response)
